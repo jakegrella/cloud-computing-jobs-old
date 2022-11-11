cd44428690b8d4f5f0b19d927d4c68ae33d35430
jake grella <jake@jakegrella.com>
2022-11-11 17:27:27 -0600

    remove component from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Component {...pageProps} />
+      {/* <Component {...pageProps} /> */}
     </div>
   );
 }
