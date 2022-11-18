deb8016b63a6c0af2251223427350373b688f92a
jake grella <jake@jakegrella.com>
2022-11-17 20:31:30 -0600

    remove authentication

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,12 @@ export function middleware(req: NextRequest, res: NextResponse) {
   const auth = req.headers.get("authorization");
 
   // not great
-  if (auth !== `Bearer ${process.env.API_SECRET_KEY}`) {
-    return new NextResponse(JSON.stringify({ message: "unauthorized" }), {
-      status: 401,
-      headers: { "content-type": "application/json" },
-    });
-  }
+  // if (auth !== `Bearer ${process.env.API_SECRET_KEY}`) {
+  //   return new NextResponse(JSON.stringify({ message: "unauthorized" }), {
+  //     status: 401,
+  //     headers: { "content-type": "application/json" },
+  //   });
+  // }
 
   return NextResponse.next();
 }
