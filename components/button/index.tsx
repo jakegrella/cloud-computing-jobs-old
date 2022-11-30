62f2ab848d9be5fe12773adcf6300243171ea102
jake grella <jake@jakegrella.com>
2022-11-30 17:12:32 -0600

    fix: give Button onClick param default value

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./button.module.css";
 
-export function Button({ children, onClick }) {
+export function Button({ children, onClick = undefined }) {
   return (
     <button className={styles.button} onClick={onClick}>
       {children}
