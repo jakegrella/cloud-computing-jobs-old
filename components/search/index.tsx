bef3ea218683126443250ca4e153bce38da6608e
jake grella <jake@jakegrella.com>
2022-11-29 20:58:43 -0600

    feat: search input dropdown design

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -2,6 +2,7 @@ import { Card } from "../card";
 import { useStore } from ".././../store";
 import { useWindowDimensions } from "../../utils/hooks";
 import styles from "./search.module.css";
+import { useEffect, useRef, useState } from "react";
 
 function ButtonGroup() {
   const homePageView = useStore((state) => state.homePageView);
@@ -26,11 +27,76 @@ function ButtonGroup() {
 export function Search({ className = undefined, placeholder = "Search" }) {
   const { width } = useWindowDimensions();
 
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [searchInputValue, setSearchInputValue] = useState("");
+
+  const searchInput = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log("document", document.activeElement);
+    }
+  }, []);
+
+  function handleInputFocus() {
+    setDropdownVisible(true);
+  }
+
+  function handleInputBlur(e) {
+    console.log("x", e.relatedTarget?.innerHTML);
+    if (
+      e.relatedTarget?.innerHTML === "city:" ||
+      e.relatedTarget?.innerHTML === "company:" ||
+      e.relatedTarget?.innerHTML === "job title:"
+    ) {
+      // add that to value of search input
+      // reset focus to input
+      searchInput.current.focus();
+    } else {
+      setDropdownVisible(false);
+    }
+  }
+
+  function handleSearchInputValueChange(e) {
+    setSearchInputValue(e.target.value);
+  }
+
+  function handleDropdownFilterClick(e) {
+    console.log("filter clicked", e);
+
+    const x = `${e.target.innerHTML} ${searchInputValue}`;
+    setSearchInputValue(x);
+  }
+
   return (
     <div className={styles.search}>
-      <Card className={`${styles.search_bar} ${className ?? ""}`}>
-        <input type="text" placeholder={placeholder} />
-      </Card>
+      {dropdownVisible ? (
+        <Card className={`${styles.search_bar} ${className ?? ""}`}>
+          <input
+            type="text"
+            ref={searchInput}
+            placeholder={placeholder}
+            value={searchInputValue}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            onChange={handleSearchInputValueChange}
+          />
+          <Card className={styles.dropdown}>
+            <button onClick={handleDropdownFilterClick}>city:</button>
+            <button onClick={handleDropdownFilterClick}>company:</button>
+            <button onClick={handleDropdownFilterClick}>job title:</button>
+          </Card>
+        </Card>
+      ) : (
+        <Card className={`${styles.search_bar} ${className ?? ""}`}>
+          <input
+            type="text"
+            placeholder={placeholder}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+          />
+        </Card>
+      )}
       {width < 768 && <ButtonGroup />}
     </div>
   );
