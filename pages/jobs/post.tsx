5efb32a210d8feae93c152fa3c280a8a4ace83be
Jake Grella <jake@jakegrella.com>
2023-03-17 14:42:51 -0700

    update spelling

--- pages/jobs/post.tsx
+++ pages/jobs/post.tsx
@@ -487,7 +487,7 @@ export default function PostAJob() {
       <div>
         <h2>Posting a Job</h2>
         <p>
-          If you would like to post a job to our job, please email us at{" "}
+          If you would like to post a job to our board, please email us at{" "}
           <a href="mailto:post@cloudcomputingjobs.com">
             post@cloudcomputingjobs.com
           </a>
