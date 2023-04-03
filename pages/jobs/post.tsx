93410c3499eb0b25590bb44892e8542c3272cb0b
jake grella <jake@jakegrella.com>
2023-04-03 10:27:47 -0700

    add /jobs/post

---
+++ pages/jobs/post.tsx
@@ -0,0 +1,14 @@
+export default function Post() {
+  return (
+    <div>
+      <h2>Posting a Job</h2>
+      <p>
+        If you would like to post a job to our board, please email us at{" "}
+        <a href="mailto:post@cloudcomputingjobs.com">
+          post@cloudcomputingjobs.com
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
