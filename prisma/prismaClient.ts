a3cb06221f39e6e8cfe85631b6300080d3c907b7
jake grella <jake@jakegrella.com>
2023-04-05 15:42:00 -0700

    addd binaryTargets to prisma generator

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -8,4 +8,4 @@ declare global {
 
 export const prisma = global.prisma || new PrismaClient();
 
-// if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
