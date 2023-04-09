f66d3dfb63f3838a8eee865e2ead31fbd861e3f8
jake grella <jake@jakegrella.com>
2023-04-09 13:24:28 -0700

    add and remove map markers on map movement

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useStore } from "../../store";
 import { ILocation } from "../../types";
 import styles from "./map.module.css";
@@ -13,17 +13,31 @@ interface MapProps {
   zoom: number;
 }
 
+function locationArraysEqual(a: ILocation[], b: ILocation[]) {
+  if (!a.length && !b.length) return true;
+  if (a.length !== b.length) return false;
+
+  // create new arrays of sorted location ids
+  const aIds = a.map((i) => i.id).sort();
+  const bIds = b.map((i) => i.id).sort();
+
+  // compare values
+  return aIds.every((val, idx) => val === bIds[idx]);
+}
+
 export function Map({ center, locations, zoom }: MapProps) {
-  const [mapBounds, setMapBounds] = useStore((state) => [
-    state.mapBounds,
-    state.setMapBounds,
-  ]);
+  const [setMapBounds] = useStore((state) => [state.setMapBounds]);
+
+  const [storedLocations, setStoredLocations] = useState<ILocation[]>([]);
+  const [markers, setMarkers] = useState<
+    { id: ILocation["id"]; marker: mapboxgl.Marker }[]
+  >([]);
 
   // React ref to store a reference to the DOM node that will be used
   // as a required parameter `container` when initializing the mapbox-gl
   // will contain `null` by default
   const mapContainer = useRef(null);
-  const map = useRef(null);
+  const map = useRef<mapboxgl.Map>(null);
 
   // init map
   useEffect(() => {
@@ -55,28 +69,84 @@ export function Map({ center, locations, zoom }: MapProps) {
         setMapBounds(bounds);
       }, 500);
     });
-
-    locations.map((location) => {
-      // marker
-      // todo: remove markers off screen
-      const marker = new mapboxgl.Marker({
-        color: "#0171e3",
-      })
-        .setLngLat([location.longitude, location.latitude])
-        .setPopup(
-          new mapboxgl.Popup({
-            className: styles.popup,
-            closeButton: false,
-            offset: [0, -15],
-          })
-            .setHTML(`<p>${location.company.name}</p>`)
-            .setMaxWidth("300px")
-        )
-        .addTo(map.current);
-
-      return marker;
-    });
   });
+
+  useEffect(() => {
+    const locationsChanged = !locationArraysEqual(locations, storedLocations);
+    // if mappable locations have changed
+    if (locationsChanged) {
+      let alreadyMarked = [];
+      let toAdd = [];
+      let toRemove = [];
+
+      // determine which locations are already marked
+      locations.forEach((location) => {
+        if (markers.some((marker) => marker.id === location.id)) {
+          alreadyMarked.push(location.id);
+        }
+      });
+
+      // determine which locations are yet to be marked
+      locations.forEach((location) => {
+        if (!alreadyMarked.some((id) => id === location.id)) {
+          toAdd.push(location);
+        }
+      });
+
+      // determine which marked locations need to be removed
+      storedLocations.forEach((location) => {
+        if (!alreadyMarked.some((id) => id === location.id)) {
+          toRemove.push(location.id);
+        }
+      });
+
+      if (toRemove.length) {
+        toRemove.forEach((id) => {
+          const markerToRemove = markers.find((marker) => marker.id === id);
+          const markerToRemoveIdx = markers.findIndex(
+            (marker) => marker.id === id
+          );
+
+          if (markerToRemove) {
+            // remove marker from map
+            markerToRemove.marker.remove();
+          }
+
+          if (markerToRemoveIdx) {
+            // remove marker from state
+            setMarkers(markers.splice(markerToRemoveIdx, 1));
+          }
+        });
+      }
+
+      const localMarkers = [...alreadyMarked, ...toAdd].map((location) => {
+        // create marker
+        const marker = new mapboxgl.Marker({
+          color: "#0171e3",
+        })
+          .setLngLat([location.longitude, location.latitude])
+          .setPopup(
+            new mapboxgl.Popup({
+              className: styles.popup,
+              closeButton: false,
+              offset: [0, -15],
+            })
+              .setHTML(`<p>${location.company.name}</p>`)
+              .setMaxWidth("300px")
+          )
+          .addTo(map.current);
+
+        // add id to marker info to allow identification
+        return { id: location.id, marker };
+      });
+
+      // update markers in state
+      setMarkers(localMarkers);
+    }
+
+    // set new locations to stored locations
+    setStoredLocations(locations);
+  }, [locations]);
 
   return (
     <div
